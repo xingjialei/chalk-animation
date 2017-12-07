9429fb0967e0ef449f62f8e2cce1ffa02acad001
xingjialei <2461539948@qq.com>
2017-12-07 16:08:58 +0800

    annotation

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 'use strict';
 
-const chalk = require('chalk');
-const gradient = require('gradient-string');
+const chalk = require('chalk');              //chalk模块引用，命令行彩色输出
+const gradient = require('gradient-string'); //gradient-string模块引用，渐变
 
 const log = console.log;
-let currentAnimation = null;
+let currentAnimation = null;                 //定义一个animation变量，在animateString函数中对其进行赋值
 
-const consoleFunctions = {
+const consoleFunctions = {                   //控制台输出对象的定义
 	log: log.bind(console),
 	info: console.info.bind(console),
 	warn: console.warn.bind(console),
@@ -24,13 +24,16 @@ for (const k in consoleFunctions) {
 const glitchChars = 'x*0987654321[]0-~@#(____!!!!\\|?????....0000\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t';
 const longHsv = {interpolation: 'hsv', hsvSpin: 'long'};
 
+//定义effects对象，彩色动画available animation(rainbow,pulse,gradient,glitch,neon)
+
 const effects = {
-	rainbow(str, frame) {
+	rainbow(str, frame) {                  //hsv是一种颜色空间
 		const hue = 5 * frame;
 		const leftColor = {h: hue % 360, s: 1, v: 1};
-		const rightColor = {h: (hue + 1) % 360, s: 1, v: 1};
+		const rightColor = {h: (hue + 1) % 360, s: 1, v: 1}; 
 		return gradient(leftColor, rightColor)(str, longHsv);
 	},
+
 	pulse(str, frame) {
 		frame = (frame % 120) + 1;
 		const transition = 6;
@@ -46,7 +49,6 @@ const effects = {
 		}
 
 		frame = frame >= transition + duration ? (2 * transition) + duration - frame : frame; // Revert animation
-
 		const g = frame <= transition / 2 ?
 			gradient([
 				{color: off, pos: 0.5 - (frame / transition)},
@@ -62,11 +64,11 @@ const effects = {
 
 		return g(str);
 	},
+
 	glitch(str, frame) {
 		if ((frame % 2) + (frame % 3) + (frame % 11) + (frame % 29) + (frame % 37) > 52) {
 			return str.replace(/[^\r\n]/g, ' ');
 		}
-
 		const chunkSize = Math.max(3, Math.round(str.length * 0.02));
 		const chunks = [];
 
@@ -92,6 +94,7 @@ const effects = {
 
 		return result;
 	},
+
 	radar(str, frame) {
 		const depth = Math.floor(Math.min(str.length, str.length * 0.2));
 		const step = Math.floor(255 / depth);
@@ -111,69 +114,78 @@ const effects = {
 
 		return chars.join('');
 	},
+
+//neon霓虹灯效果
 	neon(str, frame) {
-		const color = (frame % 2 === 0) ? chalk.dim.rgb(88, 80, 85) : chalk.bold.rgb(213, 70, 242);
+		const color = (frame % 2 === 0) ? chalk.dim.rgb(88, 80, 85) : chalk.bold.rgb(213, 70, 242); //样式修饰符dim(暗淡),bold(明亮);如果frame%2为0则字体为暗淡颜色，如果不为0则是明亮粉色
 		return color(str);
 	}
 };
 
-function animateString(str, effect, delay, speed) {
-	stopLastAnimation();
+//Defined a function called animateString
+/* @param:[String]str
+   @param:[Function]effect
+   @param:[Number]delay
+   @param:[String]speed
+   @param:return{Object} */
 
+function animateString(str, effect, delay, speed) {
+	stopLastAnimation();//调用stopLastAnimation函数，进行清除上一次的动画效果，停止动画
 	speed = speed === undefined ? 1 : parseFloat(speed);
 	if (!speed || speed <= 0) {
 		throw new Error(`Expected \`speed\` to be an number greater than 0`);
 	}
 
+  //对currentAnimation进行赋值
 	currentAnimation = {
-		text: str,
-		stopped: false,
-		init: false,
-		f: 0,
-		render() {
+		text: str,                  //动画字符
+		stopped: false,             //是否停止动画
+		init: false,                //初始化
+		f: 0,                       //
+		render() {                  //显示第一帧动画
 			const self = this;
-			if (!this.init) {
+			if (!this.init) {         //对此对象进行init初始化
 				log('');
 				this.init = true;
 			}
-			log(this.frame());
+			log(this.frame());        //调用frame()函数，进行获取下一帧
 			setTimeout(() => {
 				if (!self.stopped) {
-					self.render();
+					self.render();        //如果stopped为false,则循环调用render()函数
 				}
 			}, delay / speed);
 		},
 		frame() {
-			return '\u001B[1F\u001B[G\u001B[2K' + effect(this.text, this.f++);
+			return '\u001B[1F\u001B[G\u001B[2K' + effect(this.text, this.f++);                          //获取下一帧
 		},
-		replace(str) {
+		replace(str) {              //改变动画文本
 			this.text = str;
 		},
-		stop() {
+		stop() {                    //停止动画
 			this.stopped = true;
 			return this;
 		},
-		start() {
+		start() {                   //开始动画
 			this.stopped = false;
 			this.render();
 			return this;
 		}
 	};
-	setTimeout(() => {
+	setTimeout(() => {            //自动开始动画
 		if (!currentAnimation.stopped) {
 			currentAnimation.start();
 		}
 	}, delay / speed);
-	return currentAnimation;
+	return currentAnimation;    
 }
 
-function stopLastAnimation() {
+function stopLastAnimation() {  //停止动画
 	if (currentAnimation) {
 		currentAnimation.stop();
 	}
 }
 
-module.exports = {
+module.exports = {              //利用箭头函数进行模块暴露
 	rainbow: (str, speed) => animateString(str, effects.rainbow, 15, speed),
 	pulse: (str, speed) => animateString(str, effects.pulse, 16, speed),
 	glitch: (str, speed) => animateString(str, effects.glitch, 55, speed),
