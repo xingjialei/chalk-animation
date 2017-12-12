435b46e9cc3ab77806068fa90deabe148550a77c
xingjialei <2461539948@qq.com>
2017-12-12 14:57:13 +0800

    twice commite

--- index.js
+++ index.js
@@ -27,29 +27,33 @@ const longHsv = {interpolation: 'hsv', hsvSpin: 'long'};
 //定义effects对象，彩色动画available animation(rainbow,pulse,gradient,glitch,neon)
 
 const effects = {
+  //rainbow彩虹效果
 	rainbow(str, frame) {                  //hsv是一种颜色空间
-		const hue = 5 * frame;
+		const hue = 5 * frame;               //hue代表色调,0-360度,控制动画颜色跨度(乘5)
 		const leftColor = {h: hue % 360, s: 1, v: 1};
-		const rightColor = {h: (hue + 1) % 360, s: 1, v: 1}; 
+		const rightColor = {h: (hue + 1) % 360, s: 1, v: 1};
+    log(frame);
 		return gradient(leftColor, rightColor)(str, longHsv);
 	},
 
+  //pulse脉搏跳动效果
 	pulse(str, frame) {
-		frame = (frame % 120) + 1;
+		frame = (frame % 120) + 1;           //控制心跳频率
 		const transition = 6;
 		const duration = 10;
-		const on = '#ff1010';
-		const off = '#e6e6e6';
+		const on = '#ff1010';                //红色
+		const off = '#e6e6e6';               //白色
 
-		if (frame >= (2 * transition) + duration) {
+  //对frame进行数值判断，对文本进行效果变换
+		if (frame >= (2 * transition) + duration) {   //frame>=22
 			return chalk.hex(off)(str); // All white
 		}
-		if (frame >= transition && frame <= transition + duration) {
+		if (frame >= transition && frame <= transition + duration) {   // 6<=frame<=16
 			return chalk.hex(on)(str); // All red
 		}
 
-		frame = frame >= transition + duration ? (2 * transition) + duration - frame : frame; // Revert animation
-		const g = frame <= transition / 2 ?
+		frame = frame >= transition + duration ? (2 * transition) + duration - frame : frame; // Revert animation,如果frame>=16,则frame=22-frame;否则frame=frame(0-6的效果和16-22的效果相同)
+		const g = frame <= transition / 2 ?   
 			gradient([
 				{color: off, pos: 0.5 - (frame / transition)},
 				{color: on, pos: 0.5},
@@ -60,11 +64,12 @@ const effects = {
 				{color: on, pos: 1 - (frame / transition)},
 				{color: on, pos: frame / transition},
 				{color: off, pos: 1}
-			]);
+			]);              //定义g效果，如果frame<=3进行效果一渲染，如果3<frame<6进行效果二渲染
 
 		return g(str);
 	},
 
+  //glitch干扰效果
 	glitch(str, frame) {
 		if ((frame % 2) + (frame % 3) + (frame % 11) + (frame % 29) + (frame % 37) > 52) {
 			return str.replace(/[^\r\n]/g, ' ');
@@ -95,6 +100,7 @@ const effects = {
 		return result;
 	},
 
+  //radar雷达效果
 	radar(str, frame) {
 		const depth = Math.floor(Math.min(str.length, str.length * 0.2));
 		const step = Math.floor(255 / depth);
